d361aea83663c403da047311ba7b815fab2892f8
Sudarsan <sudar.theone@gmail.com>
2016-02-24 16:16:18 +0530

    first commit

---
+++ src/serv/main.go
@@ -0,0 +1,18 @@
+package main
+
+import("net/http"
+	"os"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("app/src/serv/static"))
+	port := os.Getenv("PORT")
+	//fs := http.FileServer(http.Dir("static"))
+
+	if port == "" {
+		port = "5000"	
+	}
+	
+	http.Handle("/" , fs)
+	http.ListenAndServe(":" + port , nil)
+}
